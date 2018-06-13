dcd7307f947f54ddf5e042321135533b3b781755
David Cordero <david@corderoramirez.com>
2018-06-14 01:34:21 +0200

    Add example project in objc

---
+++ Example/Objc/Example/AppDelegate.m
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.m
+//  Example
+//
+//  Created by David Cordero on 13.06.18.
+//  Copyright © 2018 David Cordero. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+@end
